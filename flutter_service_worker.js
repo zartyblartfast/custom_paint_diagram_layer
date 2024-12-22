eb0e3125d82f5b0292c89261efdacd80bdb8dd1f
zartyblartfast <zarty_blartfast@yahoo.com>
2024-12-22 15:45:50 +0000

    Update web app - 2024-12-22 15:45:50

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,7 +22,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "c8f6d98ca1a0a97729be769ae7bc0a01",
+"flutter_bootstrap.js": "44a49dd4d2563b38ebfa3c93ca966a2c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
